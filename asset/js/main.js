eac93da78b42cb870a96987dca48a944daef16b5
Alessandro <marzucoalessandro1@gmail.com>
2020-11-16 16:45:36 +0100

    variabili del form

---
+++ asset/js/main.js
@@ -0,0 +1,21 @@
+var passeggero = document.getElementById('passeggero').value;
+console.log(passeggero);
+var distanza = document.getElementById('distanza').value;
+console.log(distanza);
+var minorenne = document.getElementById('minorenne').value;
+console.log(minorenne);
+var maggiorenne = document.getElementById('maggiorenne').value;
+console.log(maggiorenne);
+var over = document.getElementById("over").value;
+console.log(over);
+
+var button_on = document.getElementById('genera');
+var button_reset = document.getElementById('annulla');
+
+button_on.addEventListener("click", function () {
+
+}
+
+button_reset.addEventListener("click", function () {
+
+}
