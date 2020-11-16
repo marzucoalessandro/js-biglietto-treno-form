728dc01d07afefd5ab0b2680c8b9b9991be8e00c
Alessandro <marzucoalessandro1@gmail.com>
2020-11-16 17:05:51 +0100

    elemnti del form

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,20 +2,24 @@ var passeggero = document.getElementById('passeggero').value;
 console.log(passeggero);
 var distanza = document.getElementById('distanza').value;
 console.log(distanza);
-var minorenne = document.getElementById('minorenne').value;
-console.log(minorenne);
-var maggiorenne = document.getElementById('maggiorenne').value;
-console.log(maggiorenne);
-var over = document.getElementById("over").value;
-console.log(over);
+var age = document.getElementById('età').value;
+
 
 var button_on = document.getElementById('genera');
 var button_reset = document.getElementById('annulla');
 
 button_on.addEventListener("click", function () {
 
-}
+  var passeggero = document.getElementById('passeggero').value;
+
+  var distanza = document.getElementById('distanza').value;
+
+  var age = document.getElementById('età');
+
+  console.log(passeggero, distanza, age);
+
+});
 
 button_reset.addEventListener("click", function () {
 
-}
+});
