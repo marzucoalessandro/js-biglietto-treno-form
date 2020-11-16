866fe653bb48c64a65be25458b84df2dc9dd75ce
Alessandro <marzucoalessandro1@gmail.com>
2020-11-16 21:38:16 +0100

    non appaiono le variabili nel biglietto..error js

--- asset/js/main.js
+++ asset/js/main.js
@@ -14,13 +14,13 @@ var button_reset = document.getElementById('annulla');
 //eventi scaturiti dal bottone GENERA
 button_on.addEventListener("click", function () {
 
-  var passeggero = document.getElementById('passeggero').value;
+  var nomepasseggero = document.getElementById('passeggero').value;
 
   var distanza = document.getElementById('distanza').value;
 
   var age = document.getElementById('eta').value;
 
-  console.log(passeggero, distanza, age);
+  //console.log(nomepasseggero, distanza, age);
 
   var priceticket = (distanza * 0.21);
 
@@ -29,20 +29,44 @@ button_on.addEventListener("click", function () {
 
   var prezzominore = priceticket - sconto1;
   var prezzoover65 = priceticket - sconto2;
-
+  var offerta = "prezzo standard";
+  
   if (age == "minorenne") {
     prezzominore;
-    console.log(prezzominore);
+    offerta = "sconto minorenne";
+    //console.log(prezzominore);
 
   } else if (age == "over65") {
     prezzoover65;
-    console.log(prezzoover65);
+    offerta = "sconto over 65";
+    //console.log(prezzoover65);
   }
   else {
     priceticket;
-    console.log(priceticket);
+    //console.log(priceticket);
+    offerta = "prezzo standard";
   }
 });
+
+//creiamo le var per i dati del biglietto
+
+  var utente = document.getElementById('passenger');
+  var sales = document.getElementById('sconto');
+  var carrozza = document.getElementById('carrozza');
+  var codcp = document.getElementById('cod-cp');
+  var prezzofinale = document.getElementById('costobiglietto');
+
+  //inseriamo i dati nei ns contenitori per il biglietto
+
+  //utente.innerHTML = nomepasseggero;
+  //sales.innerHTML = offerta;
+  carrozza.innerHTML = Math.floor(Math.random() * 10) + 1;
+  codcp.innerHTML = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+  //prezzofinale.innerHTML = prezzominore;
+
+
+
+
 
 
 
