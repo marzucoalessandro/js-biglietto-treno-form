8663719f1f9986e3664b96148f3556309fccab0c
Alessandro <marzucoalessandro1@gmail.com>
2020-11-16 17:57:50 +0100

    primo if

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,5 @@
+//variabili degli input nel form
+
 var passeggero = document.getElementById('passeggero').value;
 console.log(passeggero);
 var distanza = document.getElementById('distanza').value;
@@ -5,10 +7,11 @@ console.log(distanza);
 var age = document.getElementById('eta').value;
 console.log(age);
 
-
+//variabili dei pulsanti nel form
 var button_on = document.getElementById('genera');
 var button_reset = document.getElementById('annulla');
 
+//eventi scaturiti dal borrone GENERA
 button_on.addEventListener("click", function () {
 
   var passeggero = document.getElementById('passeggero').value;
@@ -22,6 +25,26 @@ button_on.addEventListener("click", function () {
   var priceticket = (distanza * 0.21);
 
   console.log(priceticket);
+
+  var sconto1 = (priceticket / 100) * 20;
+  var sconto2 = (priceticket / 100) * 40;
+
+  if (age == "minorenne") {
+    priceticket = priceticket - sconto1;
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+  }
 });
 
 button_reset.addEventListener("click", function () {
