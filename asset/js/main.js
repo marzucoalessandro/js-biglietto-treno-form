9a5c1ebdc48c5ec8901aceee8306aa33a8a285e6
Alessandro <marzucoalessandro1@gmail.com>
2020-11-17 20:18:18 +0100

    modifiche al css e verifica dei dati biglietto in console

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,13 +1,6 @@
-//variabili degli input nel form
 
-var passeggero = document.getElementById('passeggero').value;
-console.log(passeggero);
-var distanza = document.getElementById('distanza').value;
-console.log(distanza);
-var age = document.getElementById('eta').value;
-console.log(age);
 
-//variabili dei pulsanti nel form
+//variabili dei pulsanti nel nostro form
 var button_on = document.getElementById('genera');
 var button_reset = document.getElementById('annulla');
 
@@ -29,60 +22,32 @@ button_on.addEventListener("click", function () {
 
   var prezzominore = priceticket - sconto1;
   var prezzoover65 = priceticket - sconto2;
-  var offerta = "prezzo standard";
-  
+
+
   if (age == "minorenne") {
-    prezzominore;
+    priceticket = priceticket - sconto1;
     offerta = "sconto minorenne";
-    //console.log(prezzominore);
+    var carrozza = Math.floor(Math.random() * 10) + 1;
+    var codcp = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+    console.log( nomepasseggero, distanza, priceticket, offerta, carrozza, codcp);
 
   } else if (age == "over65") {
-    prezzoover65;
+    priceticket = priceticket - sconto2;
     offerta = "sconto over 65";
-    //console.log(prezzoover65);
-  }
-  else {
+    var carrozza = Math.floor(Math.random() * 10) + 1;
+    var codcp = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+    console.log(nomepasseggero, distanza, priceticket, offerta, carrozza, codcp);
+  } else {
     priceticket;
-    //console.log(priceticket);
     offerta = "prezzo standard";
+    var carrozza = Math.floor(Math.random() * 10) + 1;
+    var codcp = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+    console.log(nomepasseggero, distanza, priceticket,  offerta, carrozza, codcp);
   }
+
+
 });
 
-//creiamo le var per i dati del biglietto
+//verificato che le condizioni siano giuste e che appaiano nella console;
 
-  var utente = document.getElementById('passenger');
-  var sales = document.getElementById('sconto');
-  var carrozza = document.getElementById('carrozza');
-  var codcp = document.getElementById('cod-cp');
-  var prezzofinale = document.getElementById('costobiglietto');
-
-  //inseriamo i dati nei ns contenitori per il biglietto
-
-  //utente.innerHTML = nomepasseggero;
-  //sales.innerHTML = offerta;
-  carrozza.innerHTML = Math.floor(Math.random() * 10) + 1;
-  codcp.innerHTML = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
-  //prezzofinale.innerHTML = prezzominore;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-button_reset.addEventListener("click", function () {
-
-});
+//a questo punto dobbiamo inserirle nel nostro biglietto.
